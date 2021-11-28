1668f4836fe82f181e9799a677fd311a22cf844c
Konstantin <b.konstantin.s@gmail.com>
2021-11-28 19:41:50 +0700

    Initial Commit

---
+++ orionTelecom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  orionTelecom
+//
+//  Created by admin on 11/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
